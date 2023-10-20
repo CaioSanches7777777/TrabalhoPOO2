ee4103180d6bb5fcd3b912c3e30d016f4f4a9b1b
Caio Sanches <102621072+CaioSanches7777777@users.noreply.github.com>
2023-10-20 16:07:48 -0300

    Update index.ts
    Teoria nova. (cria e atualiza todos os dados nas tabelas) (muda as regras das relações entre avaliadores, estandes, avaliações.) (um professor pode deixar mais de uma nota para o mesmo estande, alunos não podem deixar mais de uma avaliação)

--- index.ts
+++ index.ts
@@ -13,12 +13,25 @@ async function main() {
       nome: 'asasasa'
     }
   });
+  const Professor2 = await prisma.professor.create({
+    data: {
+      id: '5',
+      email: 'ossoso@gamil.com',
+      nome: 'osososo'
+    }
+  });
 
 
   const Grupo = await prisma.grupo.create({
     data: {
       id: '3',
       estandeId: '10'
+    }
+  });
+  const Grupo2 = await prisma.grupo.create({
+    data: {
+      id: '2',
+      estandeId: '11'
     }
   });
 
@@ -32,13 +45,26 @@ async function main() {
         grupoId: '3'
       }
   });
-
+  const Aluno2 = await prisma.aluno.create({
+    data: {
+      id: '4',
+      email: 'caio@gmail.com',
+      nome: 'Caio',
+      grupoId: '2'
+    }
+});
 
 
   const Estande = await prisma.estande.create({
     data: {
       id: '10',
       grupoId: '3'
+    }
+  });
+  const Estande2 = await prisma.estande.create({
+    data: {
+      id: '11',
+      grupoId: '2'
     }
   });
 
@@ -52,12 +78,118 @@ async function main() {
     }
   });
 
-  
+  const Avaliacao2 = await prisma.avaliacao.create({
+    data: {
+      id: '2',
+      estandeId: '10',
+      notaApresentacao: 8,
+      notaIdeia: 10,
+      notaProjeto: 9
+    }
+  });
 
+  const Avaliacao3 = await prisma.avaliacao.create({
+    data: {
+      id: '3',
+      estandeId: '11',
+      notaApresentacao: 10,
+      notaIdeia: 10,
+      notaProjeto: 10
+    }
+  });
+
+  const Avaliacao4 = await prisma.avaliacao.create({
+    data: {
+      id: '4',
+      estandeId: '11',
+      notaApresentacao: 8,
+      notaIdeia: 10,
+      notaProjeto: 9
+    }
+  });
+
+  const Avaliacao5 = await prisma.avaliacao.create({
+    data: {
+      id: '5',
+      estandeId: '11',
+      notaApresentacao: 8,
+      notaIdeia: 10,
+      notaProjeto: 9
+    }
+  });
 
 
 
   console.log("Resgistro ok");
+
+
+/* ---------------- */
+
+
+  const updatedGrupo = await prisma.grupo.update({
+  where: {
+      id: '3'
+    },
+    data:{
+      alunoId: '5'
+    }
+  })
+  const updatedGrupo2 = await prisma.grupo.update({
+    where: {
+        id: '2'
+      },
+      data:{
+        alunoId: '4'
+      }
+  })
+
+
+const updatedAvaliacao = await prisma.avaliacao.update({
+  where: {
+    id: '1'
+  },
+  data:{
+    alunoId: '5'
+  }
+})
+
+const updatedAvaliacao2 = await prisma.avaliacao.update({
+  where: {
+    id: '2'
+  },
+  data:{
+    professorId: '4'
+  }
+})
+
+const updatedAvaliacao3 = await prisma.avaliacao.update({
+  where: {
+    id: '3'
+  },
+  data:{
+    alunoId: '4'
+  }
+})
+
+const updatedAvaliacao4 = await prisma.avaliacao.update({
+  where: {
+    id: '4'
+  },
+  data:{
+    professorId: '4'
+  }
+})
+
+const updatedAvaliacao5 = await prisma.avaliacao.update({
+  where: {
+    id: '5'
+  },
+  data:{
+    professorId: '4'
+  }
+})
+  
+  console.log("Atualização dos dados ok");
 }
 
 main()
