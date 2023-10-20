bbf16ec9a5ff953f2b97a565a5e86c16d4f8c5da
Caio Sanches <102621072+CaioSanches7777777@users.noreply.github.com>
2023-10-20 14:34:28 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,33 +5,45 @@ const prisma = new PrismaClient();
 
 async function main() {
   
-
-    const grupo = await prisma.grupo.create({
-        data: {
-          id: '3',
-          alunoId: '5',
-          estandeId: '10'
-        }
-      });
-
-      const aluno = await prisma.aluno.create({
-        data: {
-          id: '5',
-          email: 'arthur@gmail.com',
-          nome: 'Arthur',
-          grupoId: '3'
-        }
-      });
-
-  const professor = await prisma.professor.create({
+  const Professor = await prisma.professor.create({
     data: {
       id: '4',
-      email: 'professor@gmail.com',
-      nome: 'professor'
+      email: 'assasa@gamil.com',
+      nome: 'asasasa'
     }
   });
 
-  const avaliacao = await prisma.avaliacao.create({
+
+
+  const aluno = await prisma.aluno.create({
+      data: {
+        id: '5',
+        email: 'arthur@gmail.com',
+        nome: 'Arthur'
+      }
+  });
+
+
+
+  const Grupo = await prisma.grupo.create({
+    data: {
+      id: '3',
+      alunoId: '5',
+      estandeId: '10'
+    }
+  });
+
+
+
+  const Estande = await prisma.estande.create({
+    data: {
+      id: '10',
+      grupoId: '3'
+    }
+  });
+
+
+  const Avaliacao = await prisma.avaliacao.create({
     data: {
       id: '1',
       estandeId: '10',
@@ -40,15 +52,8 @@ async function main() {
       notaProjeto: 10
     }
   });
-  
-  
 
-  const estande = await prisma.estande.create({
-    data: {
-      id: '10',
-      grupoId: '3'
-    }
-  });
+  
 
 
 
